e7bca87329ea8adc40d40edb8db778934b773b93
Davide <herrdavide@live.de>
2026-01-12 12:32:42 +0100

    I wrote a function to calculate the absorption coefficient. I got it to the point where I got no error but the computation taked ages so I stopped it earlier. We need to make it faster haha

--- Analysis/radiation_calculation.py
+++ Analysis/radiation_calculation.py
@@ -30,6 +30,7 @@ def set_up_atmosphere(temp_profile, pressure_profile, H20_profile, CO2_concentra
     :return: xarray dataset of our atmosphere profile
     '''
 
+    #
     heights = typhon.physics.pressure2height(pressure_profile)
 
     atm = xr.Dataset(
@@ -69,9 +70,7 @@ def set_up_atmosphere(temp_profile, pressure_profile, H20_profile, CO2_concentra
     }
     return atm
 
-def calculate_spectral_csa_emission(atmosphere):
-    '''Returns spectral radiance at each pressure level using ARTS'''
-
+def calculate_radiation_transfer(atmosphere):
 
     # Create a pyarts workspace
     ws = pa.Workspace()
@@ -115,15 +114,30 @@ def calculate_spectral_csa_emission(atmosphere):
     # Automatically set up the methods to compute absorption coefficients
     ws.propagation_matrix_agendaAuto()
 
+    # this doesn't raise an error but I have no idea what it does
+    abs = pa.recipe.SingleSpeciesAbsorption(species="H2O")
+
     # Set up geometry of observation
     pos = [100e3, 0, 0]
     los = [180.0, 0.0]
     ws.ray_pathGeometric(pos=pos, los=los, max_step=1000.0)
     ws.spectral_radianceClearskyEmission()
-    return ws.spectral_radiance
+    return ws.spectral_radiance, abs, ws
 
 def calculate_total_flux(spectral_radiance):
     return np.trapezoid(spectral_radiance[:, 0], FREQ_GRID) * np.pi
+
+def calculate_absorption_coefficient(absorption, pressure_profile, atmosphere, ws):
+
+    heights = typhon.physics.pressure2height(pressure_profile)
+    absorption_coefficient = []
+
+    for h in heights:
+        atm_point = ws.atmospheric_field(h, 0, 0)
+        absorption_coefficient.append(absorption(FREQ_GRID, atm_point))
+
+    absorption_coefficient = np.array(absorption_coefficient)
+    return absorption_coefficient
 
 def plot_ola(spectral_radiance, flux):
     fig, ax = plt.subplots()
@@ -157,11 +171,13 @@ def main():
     atmosphere = set_up_atmosphere(t_profile, pressure_levels, wmr_profile, MIXING_RATIO_CO2)
 
     # radiation calculations
-    spectral_radiance_toa = calculate_spectral_csa_emission(atmosphere)
+    spectral_radiance_toa, absorption, workspace = calculate_radiation_transfer(atmosphere)
     total_flux = calculate_total_flux(spectral_radiance_toa)
+    absorption_coeff = calculate_absorption_coefficient(absorption, pressure_levels, atmosphere, workspace)
+
 
     # plot results
-    plot_ola(spectral_radiance_toa, total_flux)
+    #plot_ola(spectral_radiance_toa, total_flux)
 
 if __name__ == "__main__":
     main()
